004dff7caed75fdb2f0ab3b3f4d09008aac02b22
muzimzolo <muzisoftwaredev@gmail.com>
2021-08-19 16:30:59 +0200

    initial commit

---
+++ git-command-practice/src/main/java/com/debugger/git/practice/GitCommandPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.debugger.git.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCommandPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCommandPracticeApplication.class, args);
+	}
+
+}
